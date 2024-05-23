e69d7ee76ca6c853e6030d088364123e48f0fed0
Rasmus Høy Schmidt <rasshoey@gmail.com>
2024-05-23 19:54:08 +0200

    Tilføjede Crosscutting for UnitOfWork (transaktioner))

---
+++ GTL.Crosscut/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System.Data;
+
+namespace GTL.Crosscut
+{
+    public interface IUnitOfWork
+    {
+        void Commit();
+        void Rollback();
+        void BeginTransaction(IsolationLevel isolationLevel);
+    }
+}
