e820de6b3958f38c6259dc79709ee08fd2b65a8a
Rasmus Høy Schmidt <rasshoey@gmail.com>
2024-05-30 15:05:27 +0200

    bf

--- GTL.Domain/Models/ItemEntity.cs
+++ GTL.Domain/Models/ItemEntity.cs
@@ -19,7 +19,7 @@ namespace GTL.Domain.Models
         public static Result<ItemEntity> Create(Guid itemCatalogId, bool isLendable, DateTime dateCreated, string condition) 
         {
             Ensure.That(itemCatalogId, nameof(itemCatalogId)).IsNotEmpty();
-            Ensure.That(isLendable, nameof(isLendable));
+            //Ensure.That(isLendable, nameof(isLendable));
             Ensure.That(dateCreated, nameof(dateCreated));
             Ensure.That(condition, nameof(condition)).IsNotNullOrEmpty();
             return Result.Ok(new ItemEntity(itemCatalogId, isLendable, dateCreated, condition));
